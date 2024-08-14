5b9de1f0b9bd8a5dfc89f12dfcda349d55965284
sanatcodehub <sanu97300@gmail.com>
2024-08-14 17:19:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,14 @@
 #creating terraform providers
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.57.0"
-    }
-  }
+#terraform {
+  #required_providers {
+    #aws = {
+      #source  = "hashicorp/aws"
+      #version = "~> 4.57.0"
+    #}
+  #}
 
-  required_version = "~> 1.3.0"
-}
+  #required_version = "~> 1.3.0"
+#}
 
 provider "aws" {
   region = "eu-west-1"
