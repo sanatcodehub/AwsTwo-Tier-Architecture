ef230408004a6bda13eaaaaab1c5215076a8a73d
sanatcodehub <sanu97300@gmail.com>
2024-08-14 17:53:37 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,14 +1,13 @@
-#creating RDS Database
+# Creating RDS Database
 resource "aws_db_instance" "nagesh_database" {
   allocated_storage      = 10
   engine                 = "mysql"
   engine_version         = "5.7"
   instance_class         = "db.t2.micro"
-  db_subnet_group_name   = aws_db_subnet_group.nagesh-week18-subgroup.id
-  vpc_security_group_ids = [aws_security_group.Week18-Public-SG-DB.id]
-  name                   = "nagesh_database"
-  username               = "username" #you can create the username you want. I won't be entering one because I'm pushing to Github
-  password               = "password" #you can create the password you want. I won't be entering one becayse I'm pushing to Github
+  db_subnet_group_name   = aws_db_subnet_group.nagesh_week18_subgroup.id
+  vpc_security_group_ids = [aws_security_group.nagesh_database_tier_lu.id]
+  username               = "username"  # Replace with your username
+  password               = "password"  # Replace with your password
   parameter_group_name   = "default.mysql5.7"
   skip_final_snapshot    = true
   tags = {
@@ -16,30 +15,34 @@ resource "aws_db_instance" "nagesh_database" {
   }
 }
 
-#creating private security group for Database tier
+# Creating private security group for Database tier
 resource "aws_security_group" "nagesh_database_tier_lu" {
   name        = "nagesh_database_tier_lu"
-  description = "allow traffic from SSH & HTTP"
-  vpc_id      = aws_vpc.week18-vpc.id
+  description = "Allow traffic from SSH & HTTP"
+  vpc_id      = aws_vpc.week18_vpc.id
 
   ingress {
-    from_port       = 8279 #default port is 3306. You can also use 3307 & 8279 like myself
+    from_port       = 8279  # You can use 3306 (default), 3307, or 8279
     to_port         = 8279
     protocol        = "tcp"
     cidr_blocks     = ["10.0.0.0/16"]
-    security_groups = [aws_security_group.Nagesh-sg.id]
   }
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  tags = {
+    Name = "Nagesh-Database-SG"
   }
 }
