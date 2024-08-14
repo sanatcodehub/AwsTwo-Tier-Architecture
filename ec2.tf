492467cf049c2764230b827552a9576b995c2610
sanatcodehub <sanu97300@gmail.com>
2024-08-14 17:51:09 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
-#creating EC2 instance
-resource "aws_instance" "Nagesh-web-tier1" {
-  ami                         = "ami-0432c2005d3e6a7f4" #Amazon linux 2 AMI
+# Creating EC2 instance for Web Tier 1
+resource "aws_instance" "nagesh_web_tier1" {
+  ami                         = "ami-0432c2005d3e6a7f4" # Amazon Linux 2 AMI
   key_name                    = "kmaster"
   associate_public_ip_address = true
-  subnet_id                   = aws_subnet.public-subnet1.id
+  subnet_id                   = aws_subnet.public_subnet1.id
   instance_type               = "t2.micro"
-  vpc_security_group_ids      = [aws_security_group.Week18-Public-SG-DB.id]
+  vpc_security_group_ids      = [aws_security_group.week18_public_sg_db.id]
   user_data                   = <<-EOF
         #!/bin/bash
         yum update -y
@@ -19,14 +19,14 @@ resource "aws_instance" "Nagesh-web-tier1" {
   }
 }
 
-#creating EC2 instance
-resource "aws_instance" "Nagesh-web-tier2" {
-  ami                         = "ami-0432c2005d3e6a7f4" #Amazon linux 2 AMI 
+# Creating EC2 instance for Web Tier 2
+resource "aws_instance" "nagesh_web_tier2" {
+  ami                         = "ami-0432c2005d3e6a7f4" # Amazon Linux 2 AMI
   key_name                    = "kmaster"
   associate_public_ip_address = true
-  subnet_id                   = aws_subnet.public-subnet2.id
+  subnet_id                   = aws_subnet.public_subnet2.id
   instance_type               = "t2.micro"
-  vpc_security_group_ids      = [aws_security_group.Week18-Public-SG-DB.id]
+  vpc_security_group_ids      = [aws_security_group.week18_public_sg_db.id]
   user_data                   = <<-EOF
         #!/bin/bash
         yum update -y
@@ -35,7 +35,7 @@ resource "aws_instance" "Nagesh-web-tier2" {
         systemctl enable httpd
         echo "<html><body><h1>This is Nagesh Week18 Project Tier 2 </h1></body></html>" > /var/www/html/index.html
         EOF
-    tags = {
+  tags = {
     Name = "nagesh-EC2-2"
   }
 }
