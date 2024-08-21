67f7a04115533747b29fe5130b59bfafca982dff
sanatcodehub <sanu97300@gmail.com>
2024-08-21 15:02:02 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -30,7 +30,7 @@ resource "aws_nat_gateway" "terraform_nat_gateway" {
 
 # Creating NAT Route
 resource "aws_route_table" "terraform_route_two" {
-  vpc_id = aws_vpc.week18_vpc.id
+  vpc_id = aws_vpc.terraform_vpc.id
 
   route {
     cidr_block     = "0.0.0.0/0"
@@ -46,7 +46,7 @@ resource "aws_route_table" "terraform_route_two" {
 resource "aws_subnet" "public_subnet1" {
   vpc_id                  = aws_vpc.terraform_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-west-1a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -58,7 +58,7 @@ resource "aws_subnet" "public_subnet1" {
 resource "aws_subnet" "public_subnet2" {
   vpc_id                  = aws_vpc.terraform_vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "eu-west-1b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
@@ -70,7 +70,7 @@ resource "aws_subnet" "public_subnet2" {
 resource "aws_subnet" "private_subnet1" {
   vpc_id                  = aws_vpc.terraform_vpc.id
   cidr_block              = "10.0.3.0/24"
-  availability_zone       = "eu-west-1a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = false
 
   tags = {
@@ -82,7 +82,7 @@ resource "aws_subnet" "private_subnet1" {
 resource "aws_subnet" "private_subnet2" {
   vpc_id                  = aws_vpc.terraform_vpc.id
   cidr_block              = "10.0.4.0/24"
-  availability_zone       = "eu-west-1b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = false
 
   tags = {
